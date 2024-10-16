e43921b066e9c2b1bf29c79e1f6bbe7ec9487d12
sido <kubasido7@gmail.com>
2024-10-16 18:53:35 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def main():
     while True:
         print("*===============*")
@@ -12,13 +14,24 @@ def main():
 def flashcards(choose):
     try:
         with open(f"Sets/{str(choose)}", "r") as file:
-            content = file.read()
-            print("hej")
-            print(content)
+            lines = file.readlines()
+            while True:
+                    word1, word2 = lines[random.randint(0, len(lines) - 1)].strip().lower().split(" - ")
+                    answer = input(f"* {word1} - ").lower()
+                    print("-=======================-")
+                    if answer==word2:
+                        "Poprawna odpowiedz\n"
+                    elif answer=="0":
+                        break
+                    else:
+                        print(f"Bledna odpowiedz, poprawna: {word2}\n")
+
+
     except FileNotFoundError:
         print(f"Plik o numerze {choose} nie został znaleziony")
     except IOError as e:
         print(f"Błąd przy odczycie pliku: {e}")
+
 
 if __name__ == "__main__":
     main()
